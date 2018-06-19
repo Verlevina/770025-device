6e9c94162011d96f16365a9550ec02a5ee7360cd
Verlevina <levitinava@gmail.com>
2018-06-19 17:11:50 +0300

    слайдер

--- js/mainJs.js
+++ js/mainJs.js
@@ -4,12 +4,23 @@
 var promoSlide=document.getElementsByClassName('promo-slide');
 var controlSlide=document.getElementsByClassName('control-slide');
 for(var i=0;i<controlSlide.length;i++){
-controlSlide[i].addEventListener('click',changeCLass);}
+controlSlide[i].addEventListener('click',changeCLass);
+}
 function changeCLass(){
   for(var i=0;i<controlSlide.length;i++){
     if(controlSlide[i].classList.contains('active')){
       controlSlide[i].classList.remove('active');
+      promoSlide[i].style.display='none';
+    }
+
+  }
+  for(var i=0;i<controlSlide.length;i++) {
+    if (controlSlide[i] == this) {
+      promoSlide[i].style.display = 'flex';
     }
   }
   this.classList.add('active');
+
 }
+
+
