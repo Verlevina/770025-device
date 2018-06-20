4cfe4228b96c3ba11fcf8ed5b287556355973ea2
Verlevina <levitinava@gmail.com>
2018-06-20 11:11:21 +0300

    Живые слайдеры. осталисьЖ попапы футер фильтр

--- js/mainJs.js
+++ js/mainJs.js
@@ -3,24 +3,43 @@
  */
 var promoSlide=document.getElementsByClassName('promo-slide');
 var controlSlide=document.getElementsByClassName('control-slide');
+var serviceControl=document.getElementsByClassName('service-item');
+var serviceDescription=document.getElementsByClassName('service-description');
 for(var i=0;i<controlSlide.length;i++){
-controlSlide[i].addEventListener('click',changeCLass);
+controlSlide[i].addEventListener('click',changeSlide);
 }
-function changeCLass(){
+for(var i=0;i<serviceControl.length;i++){
+  serviceControl[i].addEventListener('click',changeServiceSlide);
+}
+
+
+function changeServiceSlide(){
+  changeCLass(serviceControl,serviceDescription,this,'service-active','block');
+}
+
+function changeSlide(){
+  changeCLass(controlSlide,promoSlide,this,'active','flex');
+}
+
+
+
+
+
+
+function changeCLass(controlSlide,promoSlide,thisItem,active,display){
   for(var i=0;i<controlSlide.length;i++){
-    if(controlSlide[i].classList.contains('active')){
-      controlSlide[i].classList.remove('active');
+    if(controlSlide[i].classList.contains(active)){
+      controlSlide[i].classList.remove(active);
       promoSlide[i].style.display='none';
     }
 
   }
   for(var i=0;i<controlSlide.length;i++) {
-    if (controlSlide[i] == this) {
-      promoSlide[i].style.display = 'flex';
+    if (controlSlide[i] == thisItem) {
+      promoSlide[i].style.display = display;
     }
   }
-  this.classList.add('active');
-
+  thisItem.classList.add(active);
 }
 
 
