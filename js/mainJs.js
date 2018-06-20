8171251b47dbe83713c6e41d1b0c1bcd6018c3ad
Verlevina <levitinava@gmail.com>
2018-06-20 22:33:55 +0300

    оживление попапов мелкие правки

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,45 +1,67 @@
-/**
- * Created by Ника on 19.06.2018.
- */
-var promoSlide=document.getElementsByClassName('promo-slide');
-var controlSlide=document.getElementsByClassName('control-slide');
-var serviceControl=document.getElementsByClassName('service-item');
-var serviceDescription=document.getElementsByClassName('service-description');
-for(var i=0;i<controlSlide.length;i++){
-controlSlide[i].addEventListener('click',changeSlide);
+var promoSlide = document.getElementsByClassName('promo-slide');
+var controlSlide = document.getElementsByClassName('control-slide');
+var serviceControl = document.getElementsByClassName('service-item');
+var serviceDescription = document.getElementsByClassName('service-description');
+for (var i = 0; i < controlSlide.length; i++) {
+  controlSlide[i].addEventListener('click', changeSlide);
 }
-for(var i=0;i<serviceControl.length;i++){
-  serviceControl[i].addEventListener('click',changeServiceSlide);
+for (var i = 0; i < serviceControl.length; i++) {
+  serviceControl[i].addEventListener('click', changeServiceSlide);
 }
 
 
-function changeServiceSlide(){
-  changeCLass(serviceControl,serviceDescription,this,'service-active','block');
+function changeServiceSlide() {
+  changeCLass(serviceControl, serviceDescription, this, 'service-active', 'block');
 }
 
-function changeSlide(){
-  changeCLass(controlSlide,promoSlide,this,'active','flex');
+function changeSlide() {
+  changeCLass(controlSlide, promoSlide, this, 'active', 'flex');
 }
 
 
-
-
-
-
-function changeCLass(controlSlide,promoSlide,thisItem,active,display){
-  for(var i=0;i<controlSlide.length;i++){
-    if(controlSlide[i].classList.contains(active)){
+function changeCLass(controlSlide, promoSlide, thisItem, active, display) {
+  for (var i = 0; i < controlSlide.length; i++) {
+    if (controlSlide[i].classList.contains(active)) {
       controlSlide[i].classList.remove(active);
-      promoSlide[i].style.display='none';
+      promoSlide[i].style.display = 'none';
     }
 
   }
-  for(var i=0;i<controlSlide.length;i++) {
+  for (var i = 0; i < controlSlide.length; i++) {
     if (controlSlide[i] == thisItem) {
       promoSlide[i].style.display = display;
     }
   }
   thisItem.classList.add(active);
 }
-
-
+//попапы
+//close
+var closePopup1 = document.getElementsByClassName('close-popup')[0];
+var closePopup2 = document.getElementsByClassName('close-popup')[1];
+var mapPopup = document.getElementsByClassName('map-popup')[0];
+var writePopup = document.getElementsByClassName('write-popup')[0];
+closePopup2.addEventListener('click', function () {
+    toClosePopup(writePopup);
+  }
+);
+closePopup1.addEventListener('click', function () {
+    toClosePopup(mapPopup);
+  }
+);
+function toClosePopup(popup) {
+  popup.style.display = 'none';
+}
+//open
+var mapOpen=document.getElementsByClassName('map')[0];
+var writeOpen=document.getElementById('write-open');
+mapOpen.addEventListener('click', function () {
+    toOpenPopup(mapPopup);
+  }
+);
+writeOpen.addEventListener('click', function () {
+    toOpenPopup(writePopup);
+  }
+);
+function  toOpenPopup(popup){
+  popup.style.display='block';
+}
